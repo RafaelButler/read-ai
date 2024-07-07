6ca712e9c30f9232ad8047150a135452242f047c
Rafael Butler <rafaelbutler80@gmail.com>
2024-07-06 23:19:53 -0300

    Colocando como ficaria o card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="">
         <div class="grid items-start flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
             <div class="lg:col-span-2">
                 <div class="w-full flex flex-1 gap-2 justify-end mb-4 text-right">
@@ -39,7 +39,6 @@
                     </x-button>
                 </div>
                 <div class="max-h-[38.5rem] overflow-y-auto">
-
 
                     <div class="relative overflow-x-auto">
                         <table
@@ -115,25 +114,174 @@
             </div>
             <div class="card">
                 <div class="card-title p-4 border-b border-gray-300">
-                    <h2 class="text-xl text-gray-900 dark:text-gray-400 font-bold">Anotações</h2>
+                    <h2 class="text-lg text-gray-900 dark:text-gray-400 font-bold">Anotações</h2>
                     <p>
                         <span class="text-gray-600">Criado em</span>
                         <span class="text-gray-400">12/12/2021</span>
                     </p>
                 </div>
 
-                <div class="card-body p-4 text-gray-600 ">
-                    <p class="text-gray-500">
-                        Nenhum anotação
-                    </p>
+                <div class="card-body flex flex-col gap-3 p-4 text-gray-600 ">
+                    {{--                    <p class="text-gray-500">--}}
+                    {{--                        Nenhum anotação--}}
+                    {{--                    </p>--}}
+
+                    <div
+                        class="p-4 border-l rounded-lg h-fit bg-white hover:bg-white hover:cursor-pointer border-l-green-600 bg-secondary"
+                    >
+
+                        <h3 class="font-semibold text-gray-500">
+                            Card title
+                        </h3>
+
+                        <p class="mt-4">
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                        </p>
+
+                        <div class="flex items-center gap-2 mt-4">
+                            <p class="text-xs font-medium text-blue-500">
+                                Comentario (0)
+                            </p>
+
+                            <p class="text-xs text-foreground">
+                                Livro: Guia do Mochileiro das Galáxias
+                            </p>
+                        </div>
+
+
+                    </div>
+                    <div
+                        class="p-4 border-l rounded-lg h-fit bg-white hover:bg-white hover:cursor-pointer border-l-green-600 bg-secondary"
+                    >
+
+                        <h3 class="font-semibold text-gray-500">
+                            Card title
+                        </h3>
+
+                        <p class="mt-4">
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                        </p>
+
+                        <div class="flex items-center gap-2 mt-4">
+                            <p class="text-xs font-medium text-blue-500">
+                                Comentario (0)
+                            </p>
+
+                            <p class="text-xs text-foreground">
+                                Livro: Guia do Mochileiro das Galáxias
+                            </p>
+                        </div>
+
+
+                    </div>
+                    <div
+                        class="p-4 border-l rounded-lg h-fit bg-white hover:bg-white hover:cursor-pointer border-l-green-600 bg-secondary"
+                    >
+
+                        <h3 class="font-semibold text-gray-500">
+                            Card title
+                        </h3>
+
+                        <p class="mt-4">
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                        </p>
+
+                        <div class="flex items-center gap-2 mt-4">
+                            <p class="text-xs font-medium text-blue-500">
+                                Comentario (0)
+                            </p>
+
+                            <p class="text-xs text-foreground">
+                                Livro: Guia do Mochileiro das Galáxias
+                            </p>
+                        </div>
+
+
+                    </div>
+                    <div
+                        class="p-4 border-l rounded-lg h-fit bg-white hover:bg-white hover:cursor-pointer border-l-green-600 bg-secondary"
+                    >
+
+                        <h3 class="font-semibold text-gray-500">
+                            Card title
+                        </h3>
+
+                        <p class="mt-4">
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                        </p>
+
+                        <div class="flex items-center gap-2 mt-4">
+                            <p class="text-xs font-medium text-blue-500">
+                                Comentario (0)
+                            </p>
+
+                            <p class="text-xs text-foreground">
+                                Livro: Guia do Mochileiro das Galáxias
+                            </p>
+                        </div>
+
+
+                    </div>
+                    <div
+                        class="p-4 border-l rounded-lg h-fit bg-white hover:bg-white hover:cursor-pointer border-l-green-600 bg-secondary"
+                    >
+
+                        <h3 class="font-semibold text-gray-500">
+                            Card title
+                        </h3>
+
+                        <p class="mt-4">
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                        </p>
+
+                        <div class="flex items-center gap-2 mt-4">
+                            <p class="text-xs font-medium text-blue-500">
+                                Comentario (0)
+                            </p>
+
+                            <p class="text-xs text-foreground">
+                                Livro: Guia do Mochileiro das Galáxias
+                            </p>
+                        </div>
+
+
+                    </div>
 
 
                 </div>
                 <div class="border-separate pb-4"></div>
                 <div class="card-footer pt-4 border-t border-gray-300">
-                    <x-secondary-button>
-                        Tag
-                    </x-secondary-button>
+                    <div class="flex items-center gap-3">
+
+                        <x-secondary-button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
+                                 fill="none"
+                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                 class="lucide lucide-plus mr-2">
+                                <path d="M5 12h14"/>
+                                <path d="M12 5v14"/>
+                            </svg>
+                            Adicionar Tag
+                        </x-secondary-button>
+                        <x-secondary-button class="flex w-fite h-full">
+                            <div
+                                class="w-2 h-2 mr-2 bg-green-600 rounded-full"
+                            ></div>
+                            Add Tag
+                        </x-secondary-button>
+                        <x-secondary-button class="flex w-fite h-full">
+                            <div
+                                class="w-2 h-2 mr-2 bg-yellow-600 rounded-full"
+                            ></div>
+                            Add Tag
+                        </x-secondary-button>
+                        <x-secondary-button class="flex w-fite h-full">
+                            <div
+                                class="w-2 h-2 mr-2 bg-blue-600 rounded-full"
+                            ></div>
+                            Add Tag
+                        </x-secondary-button>
+                    </div>
                 </div>
             </div>
         </div>
